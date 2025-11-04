d5ef1abf76f37e2608fcac2cc4c1316a61ab0124
Pouyan Madanipour <pouyan.mad@gmail.com>
2025-11-04 15:12:21 +0330

    updates

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -2,9 +2,42 @@ import { useState, useContext, Fragment } from "react";
 import { NextSeo } from "next-seo";
 import classes from "./contact.module.scss";
 import logoBlack from "@/assets/logo-black.png";
+import CloseIcon from "@mui/icons-material/Close";
+import { validateEmail } from "@/services/utility";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+  const [alert, setAlert] = useState("");
+
   const headLocationLink = "https://maps.app.goo.gl/PL91GoWmYGTzhMqt6";
+  const maxWords = 150;
+
+  const currentWordCount = message.trim()
+    ? message.trim().split(/\s+/).length
+    : 0;
+  const remainingWords = maxWords - currentWordCount;
+
+  const handleSubmit = () => {
+    if (!name || !email || !message) {
+      showAlert("Required fields *");
+      return;
+    }
+    if (!validateEmail(email)) {
+      showAlert("Invalid email");
+      return;
+    }
+  };
+
+  const showAlert = (message) => {
+    setAlert(message);
+    setTimeout(() => {
+      setAlert("");
+    }, 3000);
+  };
 
   return (
     <>
@@ -31,7 +64,7 @@ export default function Contact() {
       />
       <div className={classes.container}>
         <div className={classes.text}>
-          <h3>Let's work together</h3>
+          <h3>Let's light together</h3>
           <p
             style={{
               fontFamily: "EnglishExtraLight",
@@ -78,6 +111,125 @@ export default function Contact() {
                 Get directions
               </p>
             </div>
+          </div>
+        </div>
+        <div className={classes.formBox}>
+          <h4>Submit your inquiry</h4>
+          <div className={classes.form}>
+            <div className={classes.input}>
+              <div className={classes.bar}>
+                <p className={classes.label}>
+                  Name
+                  <span>*</span>
+                </p>
+                <CloseIcon
+                  className="icon"
+                  onClick={() => setName("")}
+                  sx={{ fontSize: 16 }}
+                />
+              </div>
+              <input
+                placeholder="..."
+                type="text"
+                id="name"
+                name="name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                autoComplete="off"
+              ></input>
+            </div>
+            <div className={classes.input}>
+              <div className={classes.bar}>
+                <p className={classes.label}>
+                  Email
+                  <span>*</span>
+                </p>
+                <CloseIcon
+                  className="icon"
+                  onClick={() => setEmail("")}
+                  sx={{ fontSize: 16 }}
+                />
+              </div>
+              <input
+                placeholder="..."
+                type="email"
+                id="email"
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                autoComplete="off"
+                dir="ltr"
+              />
+            </div>
+            <div className={classes.input}>
+              <div className={classes.bar}>
+                <p className={classes.label}>Phone</p>
+                <CloseIcon
+                  className="icon"
+                  onClick={() => setPhone("")}
+                  sx={{ fontSize: 16 }}
+                />
+              </div>
+              <input
+                placeholder="..."
+                type="tel"
+                id="phone"
+                name="phone"
+                onChange={(e) => setPhone(e.target.value)}
+                value={phone}
+                autoComplete="off"
+                dir="ltr"
+              />
+            </div>
+            <div className={classes.input}>
+              <div className={classes.bar}>
+                <p className={classes.label}>Subject</p>
+                <CloseIcon
+                  className="icon"
+                  onClick={() => setSubject("")}
+                  sx={{ fontSize: 16 }}
+                />
+              </div>
+              <input
+                placeholder="..."
+                type="text"
+                id="subject"
+                name="subject"
+                onChange={(e) => setSubject(e.target.value)}
+                value={subject}
+                autoComplete="off"
+              ></input>
+            </div>
+          </div>
+          <div className={classes.input}>
+            <div className={classes.bar}>
+              <p className={classes.label}>
+                Message
+                <span>*</span>
+              </p>
+              <CloseIcon
+                className="icon"
+                onClick={() => setMessage("")}
+                sx={{ fontSize: 16 }}
+              />
+            </div>
+            <textarea
+              placeholder="..."
+              type="text"
+              id="message"
+              name="message"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+              autoComplete="off"
+              maxLength={maxWords * 7.3}
+            ></textarea>
+            <p className={classes.count}>
+              {maxWords - remainingWords} / {maxWords}
+            </p>
+          </div>
+          <div className={classes.formAction}>
+            <p className={classes.alert}>{alert}</p>
+            <button onClick={() => handleSubmit()}>Submit</button>
           </div>
         </div>
       </div>
