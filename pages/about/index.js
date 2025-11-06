e093d7f380b83ba4f94f9d8bd02e8db0483f6539
Pouyan Madanipour <pouyan.mad@gmail.com>
2025-11-06 15:44:23 +0330

    update: about

--- pages/about/index.js
+++ pages/about/index.js
@@ -6,6 +6,11 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined";
 
 export default function About() {
+  const [pageType, setPageType] = useState(
+    "company" || "testimonials" || "awards" || "publications"
+  );
+  const navigation = ["company", "testimonials", "awards", "publications"];
+
   const testimonials = [
     {
       title: "AFDON Construction Company",
@@ -127,119 +132,175 @@ export default function About() {
         robots="index, follow"
       />
       <div className={classes.container}>
-        <div className={classes.info}>
-          <h3>Turnkey Creative Solutions</h3>
-          <p
-            style={{
-              fontFamily: "EnglishLight",
-            }}
-          >
-            Inspiration Design is a turnkey design firm founded in 2006,
-            providing creative design and exclusive construction management for
-            residential and commercial renovations and new builds.
-          </p>
-          <p
-            style={{
-              fontFamily: "EnglishLight",
-            }}
-          >
-            The firm has grown into a full-service provider offering building
-            design, interior design and lighting design. Inspiration Design has
-            successfully completed over 40 diverse projects; including high-end
-            homes, hotels, and retail spaces by working closely with top
-            international architects, designers, and builders.
-          </p>
-          <p
-            style={{
-              fontFamily: "EnglishLight",
-            }}
-          >
-            Founder Parastoo Jafari drives the firm with her comprehensive
-            experience as a building designer with advanced education in
-            architecture and engineering. She holds a Master's degree from
-            "Ecole D’Architecture de Bordeaux," France, and a Ph.D. in
-            architecture, and is a registered architect. Her extensive
-            international design background is further strengthened by 12 years
-            of technical management experience at a leading German building
-            materials manufacturer.
-          </p>
+        <div className={classes.navigation}>
+          {navigation.map((nav, index) => (
+            <p
+              key={index}
+              className={pageType === nav ? classes.navActive : classes.nav}
+              onClick={() => setPageType(nav)}
+            >
+              {nav}
+            </p>
+          ))}
         </div>
-        <div className={classes.info}>
-          <h3>Experience & Design Philosophy</h3>
-          <p
-            style={{
-              fontFamily: "EnglishLight",
-            }}
-          >
-            Inspiration Design has a privileged history of working on diverse
-            international projects, including prestigious commissions for Royal
-            families and European Embassies in the Middle East, along with
-            high-end houses in West Vancouver.
-          </p>
-          <p
-            style={{
-              fontFamily: "EnglishLight",
-            }}
-          >
-            Our work balances a contemporary vision with the client's requested
-            aesthetic, covering a vast range of architectural styles. We
-            prioritize understanding and meeting client aesthetic and
-            operational objectives while strictly adhering to budget and
-            schedule, which has helped us maintain an extensive client base.
-          </p>
-          <p
-            style={{
-              fontFamily: "EnglishLight",
-            }}
-          >
-            We view our profession as both art and science. We are committed to
-            building healthier structures using advanced construction knowledge
-            and technology. We achieve this by consciously focusing on
-            environmental responsibility and sustainability. We carefully
-            analyze all project data; including site, orientation, building
-            codes, and materials to ensure optimal solutions for energy
-            performance, durability, ventilation, and maintenance.
-          </p>
-        </div>
-        <div className={classes.info}>
-          <h3>Testimonials</h3>
-          <div className={classes.gridLayout}>
-            {testimonials.map((item, index) => (
-              <div key={index} className={classes.item}>
-                <h5>{item.title}</h5>
-                <ArrowForwardIosIcon sx={{ fontSize: 12 }} />
-              </div>
-            ))}
+        {pageType === "company" && (
+          <>
+            <div className={classes.info}>
+              <h3>Turnkey Creative Solutions</h3>
+              <p
+                style={{
+                  fontFamily: "EnglishLight",
+                }}
+              >
+                Inspiration Design is a turnkey design firm founded in 2006,
+                providing creative design and exclusive construction management
+                for residential and commercial renovations and new builds.
+              </p>
+              <p
+                style={{
+                  fontFamily: "EnglishLight",
+                }}
+              >
+                The firm has grown into a full-service provider offering
+                building design, interior design and lighting design.
+                Inspiration Design has successfully completed over 40 diverse
+                projects; including high-end homes, hotels, and retail spaces by
+                working closely with top international architects, designers,
+                and builders.
+              </p>
+              <p
+                style={{
+                  fontFamily: "EnglishLight",
+                }}
+              >
+                Founder Parastoo Jafari drives the firm with her comprehensive
+                experience as a building designer with advanced education in
+                architecture and engineering. She holds a Master's degree from
+                "Ecole D’Architecture de Bordeaux," France, and a Ph.D. in
+                architecture, and is a registered architect. Her extensive
+                international design background is further strengthened by 12
+                years of technical management experience at a leading German
+                building materials manufacturer.
+              </p>
+            </div>
+            <div className={classes.info}>
+              <h3>Experience & Design Philosophy</h3>
+              <p
+                style={{
+                  fontFamily: "EnglishLight",
+                }}
+              >
+                Inspiration Design has a privileged history of working on
+                diverse international projects, including prestigious
+                commissions for Royal families and European Embassies in the
+                Middle East, along with high-end houses in West Vancouver.
+              </p>
+              <p
+                style={{
+                  fontFamily: "EnglishLight",
+                }}
+              >
+                Our work balances a contemporary vision with the client's
+                requested aesthetic, covering a vast range of architectural
+                styles. We prioritize understanding and meeting client aesthetic
+                and operational objectives while strictly adhering to budget and
+                schedule, which has helped us maintain an extensive client base.
+              </p>
+              <p
+                style={{
+                  fontFamily: "EnglishLight",
+                }}
+              >
+                We view our profession as both art and science. We are committed
+                to building healthier structures using advanced construction
+                knowledge and technology. We achieve this by consciously
+                focusing on environmental responsibility and sustainability. We
+                carefully analyze all project data; including site, orientation,
+                building codes, and materials to ensure optimal solutions for
+                energy performance, durability, ventilation, and maintenance.
+              </p>
+            </div>
+            <div className={classes.info}>
+              <h3>Full-Service Architectural Expertise</h3>
+              <p
+                style={{
+                  fontFamily: "EnglishLight",
+                }}
+              >
+                Inspiration Design is dedicated to utilizing the latest
+                technologies and materials in the field. The company is led by
+                founder Parastoo Jafari, who holds a PhD in Architecture and the
+                CACB certification.
+              </p>
+              <p
+                style={{
+                  fontFamily: "EnglishLight",
+                }}
+              >
+                Dr. Jafari ensures the firm remains current with advancements
+                through active membership in professional organizations,
+                including RAIC (Royal Architectural Institute of Canada), IDIBC,
+                and WIA (Women in Architecture).
+              </p>
+              <p>
+                We handle all design aspects from initial building concept
+                development and interior schemes to executing precise lighting
+                design and producing all necessary construction documentation.
+              </p>
+              <p>
+                We manage all logistical and financial phases, including
+                preliminary site studies, cost estimation, regulatory compliance
+                and complete project execution, from contract negotiations to
+                consultation and final site supervision.
+              </p>
+            </div>
+          </>
+        )}
+        {pageType === "testimonials" && (
+          <div className={classes.info}>
+            <h3>Testimonials</h3>
+            <div className={classes.gridLayout}>
+              {testimonials.map((item, index) => (
+                <div key={index} className={classes.item}>
+                  <h5>{item.title}</h5>
+                  <ArrowForwardIosIcon sx={{ fontSize: 12 }} />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className={classes.info}>
-          <h3>Awards</h3>
-          <div className={classes.gridLayout}>
-            {awards.map((item, index) => (
-              <div key={index} className={classes.award}>
-                <h5
-                  style={{
-                    fontFamily: "EnglishMedium",
-                  }}
-                >
-                  {item.year}
-                </h5>
-                <h5>{item.title}</h5>
-              </div>
-            ))}
+        )}
+        {pageType === "awards" && (
+          <div className={classes.info}>
+            <h3>Awards</h3>
+            <div className={classes.gridLayout}>
+              {awards.map((item, index) => (
+                <div key={index} className={classes.award}>
+                  <h5
+                    style={{
+                      fontFamily: "EnglishMedium",
+                    }}
+                  >
+                    {item.year}
+                  </h5>
+                  <h5>{item.title}</h5>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className={classes.info}>
-          <h3>Publications</h3>
-          <div className={classes.publicationLayout}>
-            {publications.map((item, index) => (
-              <div key={index} className={classes.publication}>
-                <h5>{item}</h5>
-                <CircleOutlinedIcon sx={{ fontSize: 12 }} />
-              </div>
-            ))}
+        )}
+        {pageType === "publications" && (
+          <div className={classes.info}>
+            <h3>Publications</h3>
+            <div className={classes.publicationLayout}>
+              {publications.map((item, index) => (
+                <div key={index} className={classes.publication}>
+                  <h5>{item}</h5>
+                  <CircleOutlinedIcon sx={{ fontSize: 12 }} />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </>
   );
