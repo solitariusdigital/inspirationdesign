06dd3ad9904f2ebcc40f346f6b4451bc668f73dc
Pouyan Madanipour <pouyan.mad@gmail.com>
2025-11-08 15:16:13 +0330

    analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link
             rel="stylesheet"
@@ -17,6 +18,21 @@ export default class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#f7f9fb" />
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-HKDGL8L6C1"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HKDGL8L6C1', {
+              page_path: window.location.pathname,
+            });
+          `}
+          </Script>
         </Head>
         <body>
           <Main />
