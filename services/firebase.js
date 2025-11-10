dfec9f22f01d4c1ce3686871cd96b72dfd7ff169
Pouyan Madanipour <pouyan.mad@gmail.com>
2025-11-10 15:08:33 +0330

    update

--- services/firebase.js
+++ services/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAaWV4fUK5ngSmfD_BwuVXd7JVWTQJMy_E",
-  authDomain: "inspirationdesign-a9691.firebaseapp.com",
-  projectId: "inspirationdesign-a9691",
-  storageBucket: "inspirationdesign-a9691.firebasestorage.app",
-  messagingSenderId: "472536439167",
-  appId: "1:472536439167:web:363f501c1c1a607c0f224a",
-  measurementId: "G-HKDGL8L6C1",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
