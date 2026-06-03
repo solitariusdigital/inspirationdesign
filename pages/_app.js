97e0c6bea919e4786a39b9568a9b2397d1711439
Pouyan Madanipour <pouyan.mad@gmail.com>
2026-06-03 12:43:25 +0330

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,13 @@ export default function App({ Component, pageProps }) {
         <Head>
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0, user-scalable=no"
+            content="width=device-width, initial-scale=1, viewport-fit=cover"
+          />
+          <meta name="theme-color" content="#f2f0f0" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
           />
         </Head>
         <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -17,13 +17,6 @@ export default class MyDocument extends Document {
           />
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
-          <meta name="theme-color" content="#f2f0f0" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="black-translucent"
-          />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <Script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-HKDGL8L6C1"
