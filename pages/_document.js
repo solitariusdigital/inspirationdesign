131d3215fb1982b93a6e10f8fbe039cf8caa1e8c
Pouyan Madanipour <pouyan.mad@gmail.com>
2026-06-03 12:12:38 +0330

    updates

--- pages/_document.js
+++ pages/_document.js
@@ -17,7 +17,13 @@ export default class MyDocument extends Document {
           />
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
-          <meta name="theme-color" content="#000000" />
+          <meta name="theme-color" content="#f2f0f0" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <Script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-HKDGL8L6C1"
