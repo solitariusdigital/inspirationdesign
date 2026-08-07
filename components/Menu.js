ca638a4c0c6eb6d3c2aa54a9732cc7976a7586e4
Pouyan Madanipour <pouyan.mad@gmail.com>
2026-08-07 20:57:47 +0330

    updates

--- components/Menu.js
+++ components/Menu.js
@@ -35,11 +35,29 @@ export default function Menu() {
 
   useEffect(() => {
     if (menuMobile) {
+      const scrollY = window.scrollY;
+      document.body.style.position = "fixed";
+      document.body.style.top = `-${scrollY}px`;
+      document.body.style.left = "0";
+      document.body.style.right = "0";
       document.body.style.overflow = "hidden";
     } else {
+      const scrollY = document.body.style.top;
+      document.body.style.position = "";
+      document.body.style.top = "";
+      document.body.style.left = "";
+      document.body.style.right = "";
       document.body.style.overflow = "";
+      if (scrollY) {
+        window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+      }
     }
+
     return () => {
+      document.body.style.position = "";
+      document.body.style.top = "";
+      document.body.style.left = "";
+      document.body.style.right = "";
       document.body.style.overflow = "";
     };
   }, [menuMobile]);
