03eb16503661de7d955cca066203f914a681ffdb
Pouyan Madanipour <pouyan.mad@gmail.com>
2026-08-11 18:01:54 +0330

    updates

--- next.config.js
+++ next.config.js
@@ -1,16 +1,31 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa");
+
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/firebasestorage\.googleapis\.com\/.*/i,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "firebase-storage-images",
+        expiration: {
+          maxEntries: 100,
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+  ],
+});
 
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
-});
-
-module.exports = nextConfig;
+export default withPWA(nextConfig);
